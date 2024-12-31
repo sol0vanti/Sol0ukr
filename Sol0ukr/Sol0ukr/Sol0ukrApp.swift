3974ef3b0742c0677000af49737f78dd6ff2c90c
Alex <110104160+sol0vanti@users.noreply.github.com>
2024-12-31 21:52:54 +0200

    Init files

---
+++ Sol0ukr/Sol0ukr/Sol0ukrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sol0ukrApp.swift
+//  Sol0ukr
+//
+//  Created by Alex Balla on 31.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sol0ukrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
